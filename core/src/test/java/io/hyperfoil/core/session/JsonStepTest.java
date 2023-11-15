3371b1a9c49ac81f20bd3104ca740971ce9bab08
Francesco Nigro <nigro.fra@gmail.com>
2023-11-15 17:24:33 +0100

    Json query superhero test

--- core/src/test/java/io/hyperfoil/core/session/JsonStepTest.java
+++ core/src/test/java/io/hyperfoil/core/session/JsonStepTest.java
@@ -15,6 +15,47 @@ import io.vertx.ext.unit.junit.VertxUnitRunner;
 
 @RunWith(VertxUnitRunner.class)
 public class JsonStepTest extends BaseScenarioTest {
+
+    @Test
+    public void queryTest() {
+        String jsonString = "{\n" +
+                "  \"hero\": {\n" +
+                "    \"name\": \"Fallback hero\",\n" +
+                "    \"level\": 1,\n" +
+                "    \"picture\": \"https://dummyimage.com/280x380/1e8fff/ffffff&text=Fallback+Hero\",\n" +
+                "    \"powers\": \"Fallback hero powers\"\n" +
+                "  },\n" +
+                "  \"villain\": {\n" +
+                "    \"name\": \"Fallback villain\",\n" +
+                "    \"level\": 45,\n" +
+                "    \"picture\": \"https://dummyimage.com/280x380/b22222/ffffff&text=Fallback+Villain\",\n" +
+                "    \"powers\": \"Fallback villain powers\"\n" +
+                "  }\n" +
+                "}";
+        scenario()
+                .initialSequence("test")
+                .step(() -> {
+                    ObjectAccess json = SessionFactory.objectAccess("json");
+                    return s1 -> {
+                        System.out.println(jsonString);
+                        json.setObject(s1, jsonString.getBytes(StandardCharsets.UTF_8));
+                        return true;
+                    };
+                })
+                .stepBuilder(new JsonStep.Builder()
+                        .fromVar("json")
+                        .query(".")
+                        .storeShortcuts().format(DataFormat.STRING).toVar("output").end())
+                .step(() -> {
+                    ReadAccess output = SessionFactory.readAccess("output");
+                    return s -> {
+                        System.out.println(output.getObject(s));
+                        return true;
+                    };
+                });
+        runScenario();
+    }
+
    @Test
    public void test() {
       scenario()
